040458fd12a70d5d213c850382c251dcbfced85e
JR <jay.r.reid@gmail.com>
2019-10-19 15:05:19 -0400

    Initial Commit

---
+++ greeting.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+  "encoding/json"
+  "fmt"
+  "net/http"
+  "log"
+)
+
+type Greeting struct {
+    Greet string
+}
+
+
+func greeting(w http.ResponseWriter, r *http.Request) {
+  // return "hello" greeting
+  log.Println("greeting service called")
+  data := Greeting{"hello"}
+  b, err := json.Marshal(data)
+  if err != nil {
+    http.Error(w, "Internal Server Error", 500)
+    return
+  }
+  w.Header().Set("Content-Type", "application/json")
+  fmt.Fprint(w, string(b))
+}
+
+
+func main() {
+  log.Println("greeting service started")
+  http.HandleFunc("/greeting", greeting)
+  http.ListenAndServe(":8081", nil)
+}
